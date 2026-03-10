91af3c0ee0f21965812caf0f18bccf9bcc9b58d1
brahimvom <brahimquaiche@gmail.com>
2026-03-10 01:12:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function onScanSuccess(decodedText, decodedResult) {
-    // ملي يسكاني، دير Vibration (إلى كان تيليفون) وصوت
+   
     console.log(`Scan Result: ${decodedText}`);
     alert("تم التسجيل: " + decodedText);
 }
 
-// إعدادات الكاميرا
+
 const config = { 
     fps: 15, 
     qrbox: { width: 250, height: 250 },
@@ -13,15 +13,14 @@ const config = {
 
 let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
 
-// تشغيل الكاميرا مع معالجة الخطأ
-html5QrcodeScanner.render(onScanSuccess, (err) => {
-    // هاد السطر كيتجاهل الأخطاء البسيطة ديال "مالقيتش كود" باش ما يتبلوكاش
 
+html5QrcodeScanner.render(onScanSuccess, (err) => {
+    
 const html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", 
     { 
         fps: 20, 
-        qrbox: { width: 200, height: 200 }, // المربع الوسطاني
+        qrbox: { width: 200, height: 200 }, 
         aspectRatio: 1.0
     }
 );
